7008fc12ba1a2165ab698f11e5c9d55ae644cdaf
Cristian <crballesterosc@unal.edu.co>
2025-09-12 14:47:12 -0500

    Ejercicio - 08-09-2025

---
+++ Ejercicio - 08-09-2025 .py
@@ -0,0 +1,70 @@
+
+
+nombre1 = str(input("Ingrese su nombre "))
+edad1 = int(input("Ingrese su edad Años"))
+conse = str("Consejo de Economia -")
+consa = str("Consejo de Amor -")
+
+if(edad1 < 18):
+    print(nombre1, edad1, "Años")
+elif(edad1 >= 18 and edad1 <20):
+    consejoeco1 = "Elabora un presupuesto mensual detallando tus ingresos y gastos para saber dónde se va tu dinero y así poder hacer ajustes"
+    print(nombre1, edad1, "Años -",conse,consejoeco1)
+elif(edad1 >= 20):
+    consejoamor1 = "Dedica tiempo a conversar sobre cualquier tema que no sea trabajo o problemas, como sueños o anhelos compartidos, para fortalecer el vínculo"
+    print(nombre1, edad1, "Años -",consa, consejoamor1)
+
+print("")
+
+nombre2 = str(input("Ingrese su nombre "))
+edad2 = int(input("Ingrese su edad "))
+if(edad2 < 18):
+    print(nombre2, edad2, "Años")
+elif(edad2 >= 18 and edad2 <20):
+    consejoeco2 = "Define metas de ahorro y comienza a ahorrar, incluso pequeñas cantidades, para poder cubrir imprevistos y alcanzar objetivos futuros"
+    print(nombre2, edad2, "Años -",conse, consejoeco2)
+elif(edad2 >= 20):
+    consejoamor2 = "Presta atención a las necesidades y sentimientos de tu pareja, validando sus emociones"
+    print(nombre2, edad2, "Años -",consa, consejoamor2)
+
+print("")
+
+nombre3 = str(input("Ingrese su nombre "))
+edad3 = int(input("Ingrese su edad "))
+if(edad3 < 18):
+    print(nombre3, edad3, "Años")
+elif(edad3 >= 18 and edad3 <20):
+    consejoeco3 = "Es crucial pagar las deudas a tiempo y, de ser posible, ser totalero (pagar el total de las tarjetas) para evitar intereses"
+    print(nombre3, edad3, "Años -",conse, consejoeco3)
+elif(edad3 >= 20):
+    consejoamor3 = "Durante las discusiones, mantén un lenguaje respetuoso y evita los insultos o menosprecios"
+    print(nombre3, edad3, "Años -",consa, consejoamor3)
+
+print("")
+
+nombre4 = str(input("Ingrese su nombre "))
+edad4 = int(input("Ingrese su edad "))
+if(edad4 < 18):
+    print(nombre4, edad4, "Años")
+elif(edad4 >= 18 and edad4 <20):
+    consejoeco4 = "Antes de comprar algo que no sea una necesidad, espera 24 o 48 horas para asegurarte de que realmente lo quieres o lo necesitas"
+    print(nombre4, edad4, "Años -",conse, consejoeco4)
+elif(edad4 >= 20):
+    consejoamor4 = "No aparentes ser alguien que no eres para satisfacer expectativas. Muestra tu autenticidad para una relación duradera"
+    print(nombre4, edad4, "Años -",consa, consejoamor4)
+
+print("")
+
+nombre5 = str(input("Ingrese su nombre "))
+edad5 = int(input("Ingrese su edad "))
+if(edad5 < 18):
+    print(nombre5, edad5, "Años")
+elif(edad5 >= 18 and edad5 <20):
+    consejoeco5 = "No dependas de una sola fuente de ingresos. Busca actividades complementarias que te generen dinero adicional, como el freelancing o la venta de productos en línea"
+    print(nombre5, edad5, "Años -",conse, consejoeco5)
+elif(edad5 >= 20):
+    consejoamor5 = "Los abrazos, tomarse de la mano al caminar o los besos son importantes gestos de intimidad que fortalecen la conexión"
+    print(nombre5, edad5, "Años -",consa, consejoamor5)
+
+print("")
+
